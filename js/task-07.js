9d91a6fad60a291ea684a2eaa047597de5e5f5f4
Romaniv Dmytro <romanivdmytro@gmail.com>
2022-12-08 16:27:21 +0200

    task 7 - change event

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const rangeInputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
-rangeInputEl.addEventListener("change", (event) => {
+rangeInputEl.addEventListener("input", (event) => {
   textEl.style.fontSize = event.currentTarget.value + "px";
 });
