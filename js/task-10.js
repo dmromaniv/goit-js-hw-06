1114a7336206cda6f4c08d1fa4461db0dae342f4
Romaniv Dmytro <romanivdmytro@gmail.com>
2022-12-08 15:27:23 +0200

    add function to create collection

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,47 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Get elements ref
+const boxesRef = document.querySelector("#boxes");
+const createBtnRef = document.querySelector("[data-create]");
+const clearBtnRef = document.querySelector("[data-destroy]");
+const inputRef = createBtnRef.previousElementSibling;
+
+// Add elements listener
+createBtnRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  createBoxes(inputRef.value);
+  inputRef.value = "";
+});
+clearBtnRef.addEventListener("click", (event) => {
+  event.preventDefault();
+  destroyBoxes();
+});
+
+// Create new elements
+function createBoxes(amount) {
+  if (amount < 1 || amount > 100) {
+    alert("Можливий діапазон значень: 1-100");
+    return;
+  }
+  let newBoxex = "";
+  let minBoxWidth = 30;
+  let minBoxHeight = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const randomColor = getRandomHexColor();
+    newBoxex += `
+    <div style="width:${minBoxWidth}px; height:${minBoxHeight}px; background-color:${randomColor}">
+    </div>
+    `;
+    minBoxWidth += 10;
+    minBoxHeight += 10;
+  }
+  boxesRef.innerHTML = newBoxex;
+}
+
+// Clear all elements
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+}
